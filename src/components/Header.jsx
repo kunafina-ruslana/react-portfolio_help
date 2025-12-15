2b0fef1717222efe253d587cf339131c4bf81e74
Кунафина Руслана Руслановна <kunafina.rr1994@okeit.edu>
2025-12-15 17:22:41 +0500

    Add heaader

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import './Header.css'
+
+function App() {
+
+  return (
+    <>
+     <header>
+        <h1>КАВЕРИНА МАРИЯ И КУНАФИНА РУСЛАНА</h1>
+        <nav>
+            <a href="/makarov">Николай Макаров</a>
+            <a href="/isaev">Максим Исаев</a>
+            <a href="/hametov">Тимур Хаметов</a>
+        </nav>
+     </header>
+        
+    </>
+  )
+}
+
+export default App
